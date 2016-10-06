3425ba21e1d815b88c9db2cff0026c4dfcdad14d
ian <ianyoung37@yahoo.com>
2016-10-05 23:12:47 -0400

    I'm not even sure what I changed that time

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public static LevelGenerator instance;
+	//dear burns
+	public List<LevelPiece> levelPrefabs = new List<LevelPiece> ();
+	//this code is more jacked
+	public Transform levelStartPoint;
+	//than JGrasp
+	public List<LevelPiece> pieces = new List<LevelPiece> () ;
+
+	void Awake () {
+		instance = this;
+	}
+
+	public void AddPiece () {
+		int randomIndex = Random.Range (0, levelPrefabs.Count);
+
+		LevelPiece piece = (LevelPiece)Instantiate (levelPrefabs [randomIndex]);
+		piece.transform.SetParent (this.transform, false);
+
+		Vector3 spawnPosition = Vector3.zero;
+
+		if (piece.Count == 0) {
+			spawnPosition = levelStartPoint.position;
+		} else {
+			spawnPosition = pieces [pieces.Count - 1].exitPoint.position;
+		}
+
+		piece.transform.position = spawnPosition;
+		pieces.Add (piece);
+
+	}
+}
