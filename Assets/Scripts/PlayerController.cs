91f471c77a207f07fd239f9bfd93ca9f60ba8645
ian <ianyoung37@yahoo.com>
2016-10-05 21:38:57 -0400

    um I guess this is my commit...

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float jumpForce = 6f;
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Awake () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			//Debug.Log ("Left Mouse Button Clicked");
+			Jump ();
+		}
+	}
+
+	void Jump () {
+		rigidBody.AddForce (Vector2.up * jumpForce, ForceMode2D.Impulse);
+	}
+}
